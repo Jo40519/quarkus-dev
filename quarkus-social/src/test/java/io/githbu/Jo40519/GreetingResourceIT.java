d599ffc089f58ea8b02e52c8393428c94d01662b
Jo40519 <jaolobato85@gmail.com>
2023-03-17 11:22:53 -0300

    commit inicial treinamento quarkus

---
+++ quarkus-social/src/test/java/io/githbu/Jo40519/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package io.githbu.Jo40519;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
